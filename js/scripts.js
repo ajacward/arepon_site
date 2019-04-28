f1e6172e95dde6e1a098226522b5c45488ae8010
ajacward <ajward@unomaha.edu>
2019-04-28 15:04:32 -0500

    refactor js code iteration

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,20 @@
 // Tabbed Menu
 function openMenu(evt, menuName) {
-  var i, x, tablinks;
-  x = document.getElementsByClassName("menu");
-  for (i = 0; i < x.length; i++) {
-     x[i].style.display = "none";
+  let menuElements = document.getElementsByClassName("menu");
+
+  for (let element of menuElements) {
+     element.style.display = "none";
   }
-  tablinks = document.getElementsByClassName("tablink");
-  for (i = 0; i < x.length; i++) {
-     tablinks[i].className = tablinks[i].className.replace(" w3-red", "");
+
+  let tablinks = document.getElementsByClassName("tablink");
+
+  for (let link of tablinks) {
+     link.className = link.className.replace(" w3-red", "");
   }
+
   document.getElementById(menuName).style.display = "block";
+
   evt.currentTarget.firstElementChild.className += " w3-red";
 }
+
 document.getElementById("myLink").click();
